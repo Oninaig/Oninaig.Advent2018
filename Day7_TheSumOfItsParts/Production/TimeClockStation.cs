3b7566ea7b07634b14aa1d3c0457ccb2cb13412a
Matthew Gianino <2964006+Oninaig@users.noreply.github.com>
2018-12-19 14:12:57 -0500

    Refactoring and naming conventions

--- Day7_TheSumOfItsParts/Production/TimeClockStation.cs
+++ Day7_TheSumOfItsParts/Production/TimeClockStation.cs
@@ -7,11 +7,18 @@ namespace Day7_TheSumOfItsParts.Production
 {
     public class TimeClockStation
     {
-        public List<Worker> ClockedInWorkers { get; private set; }
         private readonly Timer _timeClock;
-        private Guid _timerId;
+        private readonly Guid _timerId;
 
-        public delegate void ClockInAction(object sender, ElapsedEventArgs args);
+        public TimeClockStation(double interval = 1000.0)
+        {
+            _timeClock = new Timer(interval);
+            _timeClock.Elapsed += MasterElapsed;
+            _timerId = Guid.NewGuid();
+            ClockedInWorkers = new List<Worker>();
+        }
+
+        public List<Worker> ClockedInWorkers { get; }
 
 
         public event ElapsedEventHandler TimeClockTimeStep
@@ -20,30 +27,28 @@ namespace Day7_TheSumOfItsParts.Production
             remove => _timeClock.Elapsed -= value;
         }
 
-        public TimeClockStation(double interval = 1000.0)
-        {
-            this._timeClock = new Timer(interval);
-            _timeClock.Elapsed += MasterElapsed;
-            this._timerId = Guid.NewGuid();
-            this.ClockedInWorkers = new List<Worker>();
-        }
-
+        /// <summary>
+        /// This event will always be the first to subscribe to the timer elapsed event so we always have an up-to-date status of the timer as the first handler thats called when the timer elapses.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         public void MasterElapsed(object sender, ElapsedEventArgs args)
         {
             Console.WriteLine($"Timer {this} current clocked in workers: {ClockedInWorkers.Count}");
         }
 
-        public void WorkerClockIn(Worker worker, ElapsedEventHandler act)
+        public void ClockIn(Worker worker, ElapsedEventHandler act)
         {
-            if(ClockedInWorkers.Contains(worker))
-                throw new ArgumentException($"Worker {worker} can't clock in more than once before clocking out in between!");
+            if (ClockedInWorkers.Contains(worker))
+                throw new ArgumentException(
+                    $"Worker {worker} can't clock in more than once before clocking out in between!");
             ClockedInWorkers.Add(worker);
             TimeClockTimeStep += act;
         }
 
-        public void WorkerClockOut(Worker worker, ElapsedEventHandler act)
+        public void ClockOut(Worker worker, ElapsedEventHandler act)
         {
-            if(!ClockedInWorkers.Contains(worker))
+            if (!ClockedInWorkers.Contains(worker))
                 throw new ArgumentException($"Worker {worker} can't clock out without clocking in first!");
             ClockedInWorkers.Remove(worker);
             TimeClockTimeStep -= act;
@@ -52,23 +57,23 @@ namespace Day7_TheSumOfItsParts.Production
         public void Start()
         {
             Console.WriteLine($"Timeclock {this} is starting!");
-            this._timeClock.Start();
+            _timeClock.Start();
         }
 
         public void Stop()
         {
-            this._timeClock.Stop();
+            _timeClock.Stop();
         }
 
         public void Restart()
         {
-            this._timeClock.Enabled = false;
-            this._timeClock.Enabled = true;
+            _timeClock.Enabled = false;
+            _timeClock.Enabled = true;
         }
 
         public override string ToString()
         {
-            var splitId = this._timerId.ToString().Replace("{", "").Replace("}", "").Split('-');
+            var splitId = _timerId.ToString().Replace("{", "").Replace("}", "").Split('-');
             var sBuilder = new StringBuilder();
             foreach (var part in splitId)
                 sBuilder.Append(part[0]);
--- Day7_TheSumOfItsParts/Production/Worker.cs
+++ Day7_TheSumOfItsParts/Production/Worker.cs
@@ -7,68 +7,69 @@ namespace Day7_TheSumOfItsParts.Production
 {
     public class Worker
     {
-  
-        public int Id { get; }
-        public WorkingStep CurrWorkStep { get; private set; }
-        public int WorkCount { get; private set; }
-        public bool IsBusy => CurrWorkStep.RemainingWorkRequired > 0;
-
         //Workers typically know where the timeclock so they can punch in/out
         private readonly TimeClockStation TimeClock;
 
         //unique id just for my own tracking
-        private Guid _uniqueId;
+        private readonly Guid _uniqueId;
 
         public Worker(int id, TimeClockStation tClock)
         {
             Id = id;
             TimeClock = tClock;
-            WorkCount = 0;
+            TotalWorkCount = 0;
             _uniqueId = Guid.NewGuid();
         }
+
+        public int Id { get; private set; }
+        public WorkingStep CurrWorkStep { get; private set; }
+        public int TotalWorkCount { get; private set; }
+        public int WorkCount { get; private set; }
+        public bool IsBusy => CurrWorkStep.RemainingWorkRequired > 0;
 
         public void SetWork(WorkingStep workStep)
         {
             CurrWorkStep = workStep;
             CurrWorkStep.Init();
+            WorkCount = 0;
         }
 
         public void ClockIn()
         {
             Console.WriteLine($"Worker {this} is clocking in!");
-            //moving to TimeClockStation TimeClock.TimeClockTimeStep += DoWorkTimeStep;
-            TimeClock.WorkerClockIn(this, DoWorkTimeStep);
+            TimeClock.ClockIn(this, DoWorkTimeStep);
         }
 
         public void ClockOut()
         {
             Console.WriteLine($"Worker {this} is clocking out!");
-            //TimeClock.TimeClockTimeStep -= DoWorkTimeStep;
-            TimeClock.WorkerClockOut(this, DoWorkTimeStep);
+            TimeClock.ClockOut(this, DoWorkTimeStep);
         }
 
         public void DoWorkTimeStep(object sender, ElapsedEventArgs args)
         {
+            TotalWorkCount++;
             WorkCount++;
             if (CurrWorkStep.DoWork())
             {
-                Console.WriteLine($"Worker {this} is working! Current total work count: {WorkCount}. Current task required work: {CurrWorkStep.WorkRequired}. Remaining work: {CurrWorkStep.RemaingWorkPct()}");
+                Console.WriteLine(
+                    $"Worker {this} is working! Current total work count: {TotalWorkCount}. Current task required work: {CurrWorkStep.WorkRequired}. Remaining work: {CurrWorkStep.RemaingWorkPct()}");
             }
             else
             {
-                Console.Write($"Worker {this} is finished his current task. Current work count: {WorkCount}. Clocking out...");
-                this.ClockOut();
+                Console.Write(
+                    $"Worker {this} is finished his current task. Current work count: {TotalWorkCount}. Clocking out...");
+                ClockOut();
             }
-            
         }
 
         public override string ToString()
         {
-            var splitId = this._uniqueId.ToString().Replace("{", "").Replace("}", "").Split('-');
+            var splitId = _uniqueId.ToString().Replace("{", "").Replace("}", "").Split('-');
             var sBuilder = new StringBuilder();
             foreach (var part in splitId)
                 sBuilder.Append(part[0]);
-            return $"{Id} / {sBuilder.ToString()}";
+            return $"{Id} / {sBuilder}";
         }
     }
 }
