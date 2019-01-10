d994cff09a2e06117c640f7ee4cf49f8c23f3f51
Matthew Gianino <2964006+Oninaig@users.noreply.github.com>
2019-01-10 16:16:10 -0500

    Works but would be a tiny bit faster if I used structs instead of classes (or just stored the marble as an int or other value type).

--- Day9_MarbleMania/Marble.cs
+++ Day9_MarbleMania/Marble.cs
@@ -7,53 +7,205 @@ using System.Xml.Schema;
 
 namespace Day9_MarbleMania
 {
+
+    public class Player
+    {
+        public int PlayerNumber { get; private set; }
+
+        public List<Marble> KeptMarbles { get; private set; }
+
+        public UInt64 Score
+        {
+            get
+            {
+                UInt64 score = 0;
+                foreach (var marb in KeptMarbles)
+                    score += (ulong)marb.Value;
+                return score;
+            }
+        }
+
+        public Guid UniqueId { get; private set; }
+
+        public Player(int playerNum)
+        {
+            PlayerNumber = playerNum;
+            this.KeptMarbles = new List<Marble>();
+            //this.UniqueId = Guid.NewGuid();
+        }
+
+        public void KeepMarble(Marble marble)
+        {
+            KeptMarbles.Add(marble);
+        }
+
+
+    }
     public class Marble
     {
-        public bool IsCurrent { get;set; }
+        public bool IsCurrent { get; set; }
         public int Value { get; private set; }
 
-        public Guid UniqueId { get; }
 
         public Marble(int value)
         {
             Value = value;
-            this.UniqueId = Guid.NewGuid();
         }
     }
 
+    public static class LinkedListExtensions
+    {
+        public static LinkedListNode<T> CircularMoveBack<T>(this LinkedList<T> lList, LinkedListNode<T> element, int moveCount)
+        {
+            var currElement = element;
+            while (moveCount > 0)
+            {
+                if (currElement == lList.First)
+                    currElement = lList.Last;
+                else
+                    currElement = currElement.Previous;
+                moveCount--;
+
+            }
+
+            return currElement;
+        }
+    }
     public class MarbleGameBoard
     {
         public LinkedList<Marble> Board { get; }
         public Marble CurrentMarble { get; private set; }
-        public MarbleGameBoard(){ this.Board = new LinkedList<Marble>();}
+        public LinkedListNode<Marble> CurrentMarbleNode { get; private set; }
+        public Dictionary<int, Player> PlayerDict { get; private set; }
+        public InfiniteEnumerable<Player> PlayersPlayOrder { get; private set; }
+        public readonly int LastMarble;
+
+        private readonly IEnumerator<Player> _playerEnum;
+
+        #region Constructors
+
+        public MarbleGameBoard(int numPlayers, int lastMarble) : this(numPlayers)
+        {
+            LastMarble = lastMarble;
+            _playerEnum = PlayersPlayOrder.GetEnumerator();
+        }
+
+        private MarbleGameBoard(int numPlayers) : this()
+        {
+            var tempArray = new Player[numPlayers];
+            PlayerDict = new Dictionary<int, Player>();
+
+            for (int i = 0; i < numPlayers; i++)
+            {
+                tempArray[i] = new Player(i + 1);
+                PlayerDict[i + 1] = new Player(i + 1);
+            }
+
+            PlayersPlayOrder = new InfiniteEnumerable<Player>(tempArray);
+        }
+
+        private MarbleGameBoard()
+        {
+            this.Board = new LinkedList<Marble>();
+        }
+
+        #endregion
 
 
+        public Player Play()
+        {
+            if (CurrentMarble == null)
+                AddMarble(new Marble(0));
+            Console.WriteLine($"Playing game with last marble worth {LastMarble} points.");
+            for (int i = 1; i <= LastMarble; i++)
+            {
+                if (i % 100000 == 0)
+                {
+                    Console.WriteLine($"On turn {i}");
+                }
+                TakeTurn(i);
+            }
+
+            return FindWinner();
+        }
+
+        private Player FindWinner()
+        {
+            UInt64 highScore = 0;
+            Player bestPlayer = null;
+            foreach (var player in PlayerDict.Values)
+            {
+                if (player.Score > highScore)
+                {
+                    highScore = player.Score;
+                    bestPlayer = player;
+                }
+            }
+
+            return bestPlayer;
+        }
+        public void TakeTurn(int marbleNum)
+        {
+            _playerEnum.MoveNext();
+            var currPlayerOrder = _playerEnum.Current;
+
+
+            var marbleToAdd = new Marble(marbleNum);
+            if (marbleToAdd.Value % 23 == 0)
+            {
+                var currPlayer = PlayerDict[currPlayerOrder.PlayerNumber];
+                currPlayer.KeepMarble(marbleToAdd);
+
+                //remove marble 7 marbles counter-clockwise from current marble
+                var marbleToRemove = Board.CircularMoveBack(CurrentMarbleNode, 7);
+                currPlayer.KeepMarble(marbleToRemove.Value);
+
+                if (Board.Last == marbleToRemove)
+                    SetCurrentMarble(Board.First.Value, Board.First);
+                else
+                    SetCurrentMarble(marbleToRemove.Next.Value, marbleToRemove.Next);
+                Board.Remove(marbleToRemove);
+            }
+            else
+            {
+                AddMarble(marbleToAdd);
+            }
+
+        }
         private void SetCurrentMarble(Marble marble)
         {
-            if(CurrentMarble!= null)
+            if (CurrentMarble != null)
                 CurrentMarble.IsCurrent = false;
             marble.IsCurrent = true;
             CurrentMarble = marble;
         }
+
+        private void SetCurrentMarble(Marble marble, LinkedListNode<Marble> marbleNode)
+        {
+            this.CurrentMarbleNode = marbleNode;
+            SetCurrentMarble(marble);
+        }
+
         public void AddMarble(Marble marble)
         {
             if (CurrentMarble == null)
             {
-                SetCurrentMarble(marble);
-                Board.AddFirst(marble);
+                var added = Board.AddFirst(marble);
+                SetCurrentMarble(marble, added);
+
             }
             else
             {
                 if (CurrentMarble == Board.Last.Value)
                 {
-                    Board.AddAfter(Board.First, marble);
-                    SetCurrentMarble(marble);
+                    var added = Board.AddAfter(Board.First, marble);
+                    SetCurrentMarble(marble, added);
                 }
                 else
                 {
-                    var currentMarbleNode = Board.Find(CurrentMarble);
-                    Board.AddAfter(currentMarbleNode.Next, marble);
-                    SetCurrentMarble(marble);
+                    //var currentMarbleNode = Board.Find(CurrentMarble);
+                    var added = Board.AddAfter(CurrentMarbleNode.Next, marble);
+                    SetCurrentMarble(marble, added);
                 }
             }
         }
--- Day9_MarbleMania/Program.cs
+++ Day9_MarbleMania/Program.cs
@@ -10,12 +10,25 @@ namespace Day9_MarbleMania
     {
         static void Main(string[] args)
         {
-            var board = new MarbleGameBoard();
-            for (int i = 0; i < 5; i++)
-            {
-                board.AddMarble(new Marble(i));
-            }
-            board.PrintBoard();
+            //var board = new MarbleGameBoard(9, 25);
+            //var bestPlayer =board.Play();
+            //Console.WriteLine($"{board.LastMarble} - High score: {bestPlayer.Score}");
+
+            //var board2 = new MarbleGameBoard(10, 1618);
+            //var bestPlayer2 =board2.Play();
+            //Console.WriteLine($"{board2.LastMarble} - High score: {bestPlayer2.Score}");
+
+            //var board3 = new MarbleGameBoard(13,7999);
+            //var bestPlayer3 = board3.Play();
+            //Console.WriteLine($"{board3.LastMarble} - High score: {bestPlayer3.Score}");
+
+            //var puzzleBoard = new MarbleGameBoard(428, 70825);
+            //var puzzleWinner = puzzleBoard.Play();
+            //Console.WriteLine($"{puzzleBoard.LastMarble} - High score: {puzzleWinner.Score}");
+
+            var puzzleBoardP2 = new MarbleGameBoard(428, (70825 * 100));
+            var puzzleWinnerP2 = puzzleBoardP2.Play();
+            Console.WriteLine($"{puzzleBoardP2.LastMarble} - High score: {puzzleWinnerP2.Score}");
             Console.ReadLine();
         }
     }
