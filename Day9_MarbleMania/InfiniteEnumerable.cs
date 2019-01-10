0da533d1d4dbf9aebaf81e46c5bfcfa373992bfc
Matthew Gianino <2964006+Oninaig@users.noreply.github.com>
2019-01-10 14:16:25 -0500

    Get to use the infinte enumerable that I made for Dana a while back.

---
+++ Day9_MarbleMania/InfiniteEnumerable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Day9_MarbleMania
+{
+    public class InfiniteEnumerable<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> enumerable;
+
+        public InfiniteEnumerable(IEnumerable<T> enumerable)
+        {
+            this.enumerable = enumerable;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            while (true)
+                foreach (var ele in enumerable)
+                    yield return ele;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
