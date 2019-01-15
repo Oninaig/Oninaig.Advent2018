3824ce046d68dcc1637c3a1b29fb84516e187048
Matthew Gianino <2964006+Oninaig@users.noreply.github.com>
2019-01-15 17:28:26 -0500

    Day 11 part 1 complete.

--- Day11_ChronalCharge/Program.cs
+++ Day11_ChronalCharge/Program.cs
@@ -10,13 +10,19 @@ namespace Day11_ChronalCharge
     {
         static void Main(string[] args)
         {
-            var testGrid = new FuelCellGrid(18);
-            var testResult = testGrid.LargestClusterPower().TotalPower;
-            var testResultCoords = testGrid.LargestClusterPower().Cluster[0].Coordinates;
+            //var testGrid = new FuelCellGrid(18);
+            //var testResult = testGrid.LargestClusterPower().TotalPower;
+            //var testResultCoords = testGrid.LargestClusterPower().Cluster[0].Coordinates;
 
-            var testGrid2 = new FuelCellGrid(42);
-            var testResult2 = testGrid2.LargestClusterPower().TotalPower;
-            var testResultCoords2 = testGrid2.LargestClusterPower().Cluster[0].Coordinates;
+            //var testGrid2 = new FuelCellGrid(42);
+            //var testResult2 = testGrid2.LargestClusterPower().TotalPower;
+            //var testResultCoords2 = testGrid2.LargestClusterPower().Cluster[0].Coordinates;
+
+
+            var puzzleGrid = new FuelCellGrid(5719);
+            var puzzleResult = puzzleGrid.LargestClusterPower().TotalPower;
+            var puzzleResultCoords = puzzleGrid.LargestClusterPower().Cluster[0].Coordinates;
+            Console.WriteLine($"{puzzleResultCoords.X}, {puzzleResultCoords.Y}");
             Console.ReadLine();
         }
     }
