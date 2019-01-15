f8b764d81fa6a0e7fee3481c6d66bc541314b361
Matthew Gianino <2964006+Oninaig@users.noreply.github.com>
2019-01-15 11:51:39 -0500

    Day 11 Start

---
+++ Day11_ChronalCharge/FuelCell.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11_ChronalCharge
+{
+    public class FuelCell
+    {
+        public int RackId { get; private set; }
+        public int PowerLevel { get; private set; }
+        public Point Coordinates { get; set; }
+
+        public int GridSerialNumber { get; private set; }
+        public FuelCell(Point coordinates, int gridSerialNumber)
+        {
+            this.Coordinates = coordinates;
+            this.GridSerialNumber = gridSerialNumber;
+            initPowerLevel();
+        }
+
+        private void initPowerLevel()
+        {
+            this.RackId = Coordinates.X + 10;
+            var powerLevel = RackId * Coordinates.Y;
+            powerLevel += GridSerialNumber;
+            powerLevel *= RackId;
+            powerLevel = Math.Abs(powerLevel / 100 % 10);
+            powerLevel = powerLevel < 10 ? powerLevel : 0;
+            powerLevel -= 5;
+            PowerLevel = powerLevel;
+        }
+
+    }
+
+    public struct Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
---
+++ Day11_ChronalCharge/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Day11_ChronalCharge
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ExampleFuelCell()
+        {
+            var point = new Point(3, 5);
+            var serial = 8;
+            var cell = new FuelCell(point, serial);
+            Assert.AreEqual(4, cell.PowerLevel);
+        }
+    }
+
+}
