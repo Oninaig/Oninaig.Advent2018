6f9c3ef68ac9d9a113f09f2b5e39c42c0ffded53
Matthew Gianino <2964006+Oninaig@users.noreply.github.com>
2019-02-26 12:34:41 -0500

    Day 15 start and also created a common project for the types I seem to have to keep recreating

---
+++ Advent2018.Common/Navigation/Coord.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Advent2018.Common.Navigation
+{
+    public struct Coord
+    {
+        private int? _x;
+        private int? _y;
+
+        public int X
+        {
+            get => _x ?? -1;
+            set => _x = value;
+        }
+
+        public int Y
+        {
+            get => _y ?? -1;
+            set => _y = value;
+        }
+
+        public Coord(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public double DistanceFrom(Coord otherCoord)
+        {
+            var result = Math.Sqrt((otherCoord.X - X) * (otherCoord.X - X) + (otherCoord.Y - Y) * (otherCoord.Y - Y));
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherCoord = (Coord) obj;
+            return otherCoord.X == X && otherCoord.Y == Y;
+        }
+    }
+}
---
+++ Day15_BeverageBandits/Unit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15_BeverageBandits
+{
+    public class Unit
+    {
+        private IEnumerable<Unit> Targets { get; set; }
+
+    }
+
+    public class Grid
+    {
+
+    }
+
+    public struct Coord
+    {
+        public int? _x;
+        public int? _y;
+    }
+}
